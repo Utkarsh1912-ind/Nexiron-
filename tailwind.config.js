cb98628d190147d3f5e20f036c02974f49bfd012
Utkarsh1912-ind <yadavbholu571@gmail.com>
2026-06-13 22:20:05 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neon: "#39FF14",
+      },
+    },
+  },
+  plugins: [],
+};
