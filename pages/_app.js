37ea2dca1902c6f6eee4062d2ea476f31348d3f1
Utkarsh1912-ind <yadavbholu571@gmail.com>
2026-06-14 16:15:14 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-6WMFR9MVDF"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-6WMFR9MVDF');
+        `}
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
